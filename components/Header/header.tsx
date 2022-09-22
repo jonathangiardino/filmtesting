0998b8756775339919500fc71da931500af8832d
Jonathan <j.giardino@me.com>
2022-09-22 14:32:37 +0200

    Add back button in logo

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import React, { FC } from 'react'
 
 const Header: FC = () => {
   return (
     <div className="h-[70px] w-screen max-w-full border-solid border-b-[1px] border-black">
       <div className="max-w-[1330px] h-full flex items-center justify-between px-10 mx-auto">
-        <div className="uppercase text-xl tracking-[7px] font-bold flex items-center gap-[2px] -ml-1">
-          <span className="text-2xl">&bull;</span> filmtesting
-        </div>
+        <Link href={"/"}>
+          <div className="uppercase text-xl tracking-[7px] font-bold flex items-center gap-[2px] -ml-1 cursor-pointer">
+            <span className="text-2xl">&bull;</span> filmtesting
+          </div>
+        </Link>
       </div>
     </div>
   );
